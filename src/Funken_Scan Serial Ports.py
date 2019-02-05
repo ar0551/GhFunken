facdd3d1706fa0f7dd06c49b86dada456bc7fd93
AndreaRossi1988 <a.rossi.andrea@gmail.com>
2019-02-05 13:08:11 +0100

    implement port scanning for Mac

--- src/Funken_Scan Serial Ports.py
+++ src/Funken_Scan Serial Ports.py
@@ -14,24 +14,27 @@ Provided by Funken 0.3.0
 
 ghenv.Component.Name = "Funken_Scan Serial Ports"
 ghenv.Component.NickName = 'ScanPorts'
-ghenv.Component.Message = 'VER 0.3.1'
+ghenv.Component.Message = 'VER 0.3.2'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Funken"
 ghenv.Component.SubCategory = "0 | Funken"
 try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
 except: pass
 
-
-import _winreg as winreg
-import itertools
+import platform
+import Grasshopper as gh
 
 
 ## https://eli.thegreenplace.net/2009/07/31/listing-all-serial-ports-on-windows-with-python/
-def enumerate_serial_ports():
+def enumerate_serial_ports_win():
     """ Uses the Win32 registry to return an
         iterator of serial (COM) ports
         existing on this computer.
     """
+    
+    import _winreg as winreg
+    import itertools
+    
     path = 'HARDWARE\\DEVICEMAP\\SERIALCOMM'
     try:
         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
@@ -49,5 +52,22 @@ def enumerate_serial_ports():
             break
     return ports
 
+## missing reference link 
+## !!! NOT TESTED !!!
+def enumerate_serial_ports_mac():
+    import glob
+    ports = glob.glob("/dev/tty.*")
+    return ports
+
+
 if SCAN:
-    PORTS = enumerate_serial_ports()
+    os_name = platform.system()
+    
+    if os_name == "Windows":
+        PORTS = enumerate_serial_ports_win()
+    elif os_name == "Darwin":
+        PORTS = enumerate_serial_ports_mac()
+    else:
+        msg = "Your operative system is currently not supported by Funken.\n You might try to write the name of the serial port manually to the OpenPort component."
+        ghenv.Component.AddRuntimeMessage(gh.Kernel.GH_RuntimeMessageLevel.Error, msg)
+
